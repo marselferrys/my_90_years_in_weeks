a4cccb22bdec8164b5508ecf943b8b96d5fe21eb
121140195_Marchel Ferry Timoteus S <66813668+marselferrys@users.noreply.github.com>
2026-04-18 14:43:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -434,7 +434,8 @@ st.caption("Minggu 1 ➔ 52")
 # RENDER GRID KALENDER & TOMBOL CATATAN TAHUNAN
 # ==========================================
 for year in range(target_age + 1):
-    col_grid, col_spacer, col_action = st.columns([0.93, 0.02, 0.05])
+    # kolom tengah (0.02) sebagai ruang kosong/spacer
+    col_grid, col_spacer, col_action = st.columns([0.93, 0.10, 0.05])
     
     with col_grid:
         weeks_html = f'<div class="week-container"><div class="age-label">Thn {year:02d}</div>'
